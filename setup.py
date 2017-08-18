0ea62389aac708d8ae104ed5a7913d4b71ad3a5d
Tim Savage <tim@savage.company>
2017-08-18 11:53:21 +1000

    Migrated to PBR

--- setup.py
+++ setup.py
@@ -1,46 +1,7 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+#!/usr/bin/env python
+from setuptools import setup
 
 setup(
-    name='odincontrib.sqlalchemy',
-    version='0.1',
-    description="Integration with SQL Alchemy to provide custom fields and mapping support.",
-    long_description=long_description,
-    url='https://github.com/python-odin/odincontrib.sqlalchemy',
-    author='Tim Savage',
-    author_email='tim@savage.company',
-    license='BSD',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-
-        'License :: OSI Approved :: BSD License',
-
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-
-    keywords='odin web rest api sqlalchemy',
-
-    packages=find_packages(include=('odincontrib',)),
-
-    install_requires=['odin>=0.10', 'six', 'sqlalchemy>=1.1'],
+    setup_requires=['pbr>=3.0', 'setuptools>=17.1'],
+    pbr=True,
 )
